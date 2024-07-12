3fd40efebef12c5b58bfd47d22ac588bacbf59f1
Isha Hasham <144732430+ishahasham@users.noreply.github.com>
2024-07-13 01:17:41 +0500

    add login and signup file in different folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './Login'
-import Signup from './Signup'
+import Login from './Login/Login'
+import Signup from './Signup/Signup'
 
 function App() {
   let [loginForm, setLoginForm]=useState(true);
